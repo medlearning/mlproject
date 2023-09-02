09efe9cdc4273dbc765915ab02e27411ee3993ba
Medoune Fame <medoune.learning@gmail.com>
2023-09-02 16:47:31 +0200

    hyperparameter tuning

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import sys
 import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score
+from sklearn.model_selection import RandomizedSearchCV
 import dill 
 from src.exception import CustomException
 
@@ -18,14 +19,20 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            param = params[list(models.keys())[i]]
 
+            rs = RandomizedSearchCV(model, param, cv=3)
+            rs.fit(X_train, y_train)
+
+            model.set_params(**rs.best_params_)
             model.fit(X_train,y_train)
+
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
 
