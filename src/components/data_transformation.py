05204eddbf267ab440d2d5331cd5cefc2a656652
Medoune Fame <medoune.learning@gmail.com>
2023-09-04 18:49:12 +0200

    Prediction Pipeline and App building

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -15,7 +15,7 @@ from src.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
-    preprocessor_obj_file_path = os.path.join('artifacts', 'preprocessor.pickle')
+    preprocessor_obj_file_path = os.path.join('artifacts', 'preprocessor.pkl')
 
 class DataTransformation:
     def __init__(self):
--- src/utils.py
+++ src/utils.py
@@ -27,7 +27,7 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, params):
             model = list(models.values())[i]
             param = params[list(models.keys())[i]]
 
-            rs = RandomizedSearchCV(model, param, cv=3)
+            rs = RandomizedSearchCV(model, param, cv=3, )
             rs.fit(X_train, y_train)
 
             model.set_params(**rs.best_params_)
@@ -46,3 +46,10 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, params):
         raise CustomException(e,sys) 
 
 
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return dill.load(file_obj)
+        
+    except Exception as e:
+        raise CustomException(e,sys)
